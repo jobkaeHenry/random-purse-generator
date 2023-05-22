625fd70c70b36d66c3082c662ba7553381cbb495
Jungu Lee <100949102+jobkaeHenry@users.noreply.github.com>
2023-05-22 11:03:20 +0900

    :art: 랜더링최적화 requestAnimationFram()

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,25 @@ export default function App() {
   const [parsedData, setParsedData] = useState<number[]>(
     Array.from({ length: 100 }, () => 400)
   );
-  const frameRate = 16;
 
   // 새로운 데이터를 계속 서버에서 받는다는 가정으로 데이터를 생성
   useEffect(() => {
-    const interval = setInterval(() => {
+    const animate = () => {
+      requestAnimationFrame(animate);
       //랜덤 숫자를 생성
       const randomNum = Number(Math.random() * (380 - 420) + 420);
       setData({ value: randomNum });
-    }, frameRate);
-    return () => clearInterval(interval);
+    };
+    animate();
   }, []);
 
-  //서버에서 새로운 데이터를 받으면, chart에서 사용할 수 있는 형태로 변경 
+  //서버에서 새로운 데이터를 받으면, chart에서 사용할 수 있는 형태로 변경
   useEffect(() => {
     // 현재 데이터배열 길이가 60 이하일 경우, 새롭게 추가
     if (parsedData.length < 60) {
       setParsedData((prev) => [...prev, data.value]);
-    // 현재 데이터배열 길이가 60 이상일 경우 , 기존값을 제거 후 추가
-    } else if(parsedData.length>=60) {
+      // 현재 데이터배열 길이가 60 이상일 경우 , 기존값을 제거 후 추가
+    } else if (parsedData.length >= 60) {
       setParsedData((prev) => {
         const [_beforeNum, ...others] = prev;
         return [...others, data.value];
